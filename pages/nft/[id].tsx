fbc4b6a7fc040d9d524e0aa7d9861cc687f0609c
Aestheticsuraj234 <jhapriya458@gmail.com>
2022-10-03 10:56:36 +0530

    day-3.2

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import type { GetServerSideProps } from "next";
 import { useAddress, useDisconnect, useMetamask  } from "@thirdweb-dev/react";
 import { sanityClient, urlFor } from "../../sanity";
 import { Collection } from "../../typings";
